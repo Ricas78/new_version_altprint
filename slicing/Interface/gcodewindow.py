511587e6125a8c39dc59ba95b74ff0c5580555d4
Ricas78 <ricardo.dias78@hotmail.com>
2026-05-07 21:45:20 -0300

    reduzindo variaveis e comando desnecessários

--- slicing/Interface/gcodewindow.py
+++ slicing/Interface/gcodewindow.py
@@ -1,12 +1,15 @@
-import tkinter as tk
+import tkinter as tk  # biblioteca padrão de GUI do Python
+# widgets mais modernos (botões, frames, etc.)
 from tkinter import ttk, messagebox, filedialog
+# renderização gráfica (GPU), área de desenho 3D
 from vispy.scene import visuals, SceneCanvas
-from vispy.app import use_app
-import os
-import matplotlib.pyplot as plt
-import re
-import numpy as np
-import matplotlib.cm as cm
+from vispy.scene.visuals import Box
+from vispy.visuals.transforms import STTransform, MatrixTransform
+from vispy.app import use_app  # integra VisPy com Tkinter
+import os  # manipulação de arquivos
+import re  # regex (parse do G-code)
+import numpy as np  # arrays eficientes
+import matplotlib.cm as cm  # mapas de cores
 
 
 X_MAX_DEFAULT = 200
@@ -25,73 +28,87 @@ class GcodeWindow:
         self.zMaxSv = tk.StringVar(value=str(Z_MAX_DEFAULT))
         self.mapaDeCores = cm.coolwarm
         self.layerSliderVar = tk.DoubleVar()
-        self.comandosGcodeParaEnviar = []
-        self.totalMovimentos = 0
-        self.movimentosConfirmados = 0
 
+        # tela-container para: botao de abrir gcode, label do status gcode
+        self.control_frame = ttk.Frame(self.gcodewindow, padding="10")
+        self.control_frame.pack(side='top', fill='x')
+
+        # botão abrir gcode
+        self.btnLer = ttk.Button(self.control_frame, text="Abrir G-Code",
+                                 command=self.carregarGcodeVispy)
+        self.btnLer.pack(side='left', padx=5)
+
+        # label do status gcode
         self.gcodeLabelStatusSv = tk.StringVar(
             value="Nenhum arquivo carregado.")
 
-        # tela-container para o botao de abrir gcode
-        control_frame = ttk.Frame(self.gcodewindow, padding="10")
-        control_frame.pack(side='top', fill='x')
+        self.labelStatus = ttk.Label(
+            self.control_frame, textvariable=self.gcodeLabelStatusSv)
+        self.labelStatus.pack(side='left', padx=10, expand=True, fill='x')
 
-        btnLer = ttk.Button(control_frame, text="Abrir G-Code",
-                            command=self.carregarGcodeVispy)
-        btnLer.pack(side='left', padx=5)
-
-        labelStatus = ttk.Label(
-            control_frame, textvariable=self.gcodeLabelStatusSv)
-        labelStatus.pack(side='left', padx=10, expand=True, fill='x')
-
+        # tela-container para: label de n of layers, label de valor do slider e scala do slider
         self.sim_frame = ttk.Frame(self.gcodewindow, padding="10")
         self.sim_frame.pack(side='top', fill='x')
 
-        ttk.Label(self.sim_frame, text="Number of layers: ").pack(side='left')
+        # label de n of layers
+        self.layersRef = ttk.Label(self.sim_frame, text="Number of layers: ")
+        self.layersRef.pack(side='left')
 
+        # label de valor do slider
         self.layerSliderValor = ttk.Label(self.sim_frame, width=6)
         self.layerSliderValor.pack(side='right', padx=5)
 
+        # scala do slider
         self.layerSlider = ttk.Scale(self.sim_frame, from_=0, to=100, orient='horizontal',
                                      state='disabled', variable=self.layerSliderVar, command=self.simularGcodeVispy)
         self.layerSlider.pack(side='left', fill='x', expand=True)
 
-        print_frame = ttk.Frame(self.gcodewindow, padding="10")
-        print_frame.pack(side='top', fill='x')
-
-        # self.btn_imprimir = ttk.Button(
-        #     print_frame, text="Imprimir G-Code", command=self.iniciarImpressao)
-        # self.btn_imprimir.pack(side='left', padx=5)
-
-        # self.btn_cancelar = ttk.Button(
-        #     print_frame, text="Cancelar", command=self.cancelarImpressao, state='disabled')
-        # self.btn_cancelar.pack(side='left', padx=5)
-
-        # self.print_progress_var = tk.DoubleVar()
-        # self.print_progress_bar = ttk.Progressbar(
-        #     print_frame, variable=self.print_progress_var, maximum=100)
-        # self.print_progress_bar.pack(
-        #     side='left', fill='x', expand=True, padx=5)
-
+        # tela-container para: visualização do gcode
         self.plot_frame = ttk.Frame(self.gcodewindow)
         self.plot_frame.pack(side='bottom', fill='both',
                              expand=True, padx=5, pady=5)
 
+        # integra VisPy com Tkinter
         use_app('tkinter')
 
+        # visualização da animação do gcode (cria canvas 3D)
         self.vispyCanvas = SceneCanvas(
-            keys='interactive', bgcolor='black', parent=self.plot_frame)
+            keys='interactive', bgcolor='white', parent=self.plot_frame)
         self.vispyCanvas.native.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
+        # controle de camera da visualização
         self.vispy_view = self.vispyCanvas.central_widget.add_view()
-
-        self.vispy_linha_visual = visuals.Line(parent=self.vispy_view.scene)
-
         self.vispy_view.camera = 'turntable'
+        self.vispy_view.camera.up = 'z'
+        self.vispy_view.camera.azimuth = 45
+        self.vispy_view.camera.elevation = 30
         self.vispy_view.camera.fov = 45
         self.vispy_view.camera.distance = 100
 
+        # objeto que desenha as linhas do gcode
+        self.vispy_linha_visual = visuals.Line(parent=self.vispy_view.scene)
+
+# Aplicando a bed
+        # bed = Box(
+        #     width=200,
+        #     height=200,
+        #     depth=1,
+        #     color=(0.2, 0.2, 0.2, 1),  # cinza escuro
+        #     parent=self.vispy_view.scene
+        # )
+
+        # bed.transform = STTransform(
+        #     translate=(100, 100, -0.5)
+        # )
+
+        # trans = MatrixTransform()
+        # trans.rotate(90, (1, 0, 0))  # 90° em torno do eixo X
+
+        # bed.transform = trans
+
     def carregarGcodeVispy(self):
+
+        # abrir selecionador de arquivos e selecionar apenas arquivos gcode
         path = filedialog.askopenfilename(
             title="Selecionar arquivo G-Code",
             # , ("Todos os arquivos", "*.*")]
@@ -100,56 +117,46 @@ class GcodeWindow:
         if not path:
             return
 
-        self.gcodeArqPath = path
-
+        # atualiza o texto da label de status de leitura do gcode
         self.gcodeLabelStatusSv.set(f"Lendo: {os.path.basename(path)}...")
         self.mainwindow.update_idletasks()
 
+        # inicializando variaveis locais
         posicoes = []
         colors = []
-
-        self.comandosGcodeParaEnviar = []
-
         posAtual = [0.0, 0.0, 0.0]
-        tempAtual = 20.0
-        tempMin, tempMax = 20.0, 20.0
         zMax = 0.0
-
         flagComeco = True
 
-        self.tempNormalizada = plt.Normalize(vmin=20, vmax=250)
-
         try:
+            # inicio da leitura do arquivo gcode
             with open(path, 'r', encoding='utf-8') as f:
                 avisado = False
                 for linha in f:
+
+                    # remove comentários
                     linhaStrip = linha.split(';')[0].strip().upper()
 
+                    # separa linha a linha dos comandos gcode
                     comandos = linhaStrip.split()
+
+                    # se comandos tiver vazio, vai para proxima iteração do laço
                     if not comandos:
                         continue
 
-                    if "M104" in comandos or "M109" in comandos:
-                        sEncontrado = re.search(
-                            r"S(\d+(?:\.\d+)?)", linhaStrip)  # (\d+\.?\d*)
-                        if sEncontrado:
-                            tempAtual = float(sEncontrado.group(1))
-                            if tempAtual > tempMax:
-                                tempMax = tempAtual
-                            if tempAtual > 20 and (tempAtual < tempMin or tempMin == 20.0):
-                                tempMin = tempAtual
-                        pass
-
-                    movimentoFlag = False
+                    # filtra pra pegar apenas comandos de travel (G0) e extrusão (G1)
                     if "G0" in comandos or "G1" in comandos:
-                        movimentoFlag = True
+
+                        # extrai as coordenadas do comando (seja negativo ou decimal)
                         xEncontrado = re.search(r"X(\-?\d+\.?\d*)", linhaStrip)
                         yEncontrado = re.search(r"Y(\-?\d+\.?\d*)", linhaStrip)
                         zEncontrado = re.search(r"Z(\-?\d+\.?\d*)", linhaStrip)
                         eEncontrado = re.search(r"E(\-?\d+\.?\d*)", linhaStrip)
 
+                        # copia da pos atual
                         novaPos = list(posAtual)
 
+                        # extrai somente o valor numérico das coords e reescreve no array novaPos
                         if xEncontrado:
                             novaPos[0] = float(xEncontrado.group(1))
                         if yEncontrado:
@@ -157,45 +164,47 @@ class GcodeWindow:
                         if zEncontrado:
                             novaPos[2] = float(zEncontrado.group(1))
 
+                        # verifica se a alguma coordenada de gcode fora dos limites da área de impressão
                         if (novaPos[0] > abs(float(self.xMaxSv.get())) or novaPos[1] > abs(float(self.yMaxSv.get())) or novaPos[2] > abs(float(self.zMaxSv.get()))):
                             if not avisado:
                                 messagebox.showerror(
                                     "Aviso", "O G-Code carregado ultrapassa os limites da impressora")
                                 avisado = True
 
+                        # verifica se é um movimento de extrusão
                         movExtruCheck = eEncontrado and float(
                             eEncontrado.group(1)) > 0 and "G1" in comandos
 
+                        # ignora movimentos que são iguais ao anterior e n apresentam extrusão (volta pro começo do loop)
                         if novaPos == posAtual and not eEncontrado:
                             continue
 
+                        # ignora o primeiro ponto pq ainda n existe segmento anterior (descarta o ponto 0,0,0 colocado pra iniciar a var)
                         if flagComeco:
                             flagComeco = False
                         else:
+                            # adiciona o par de pontos de cada segmento
                             posicoes.append(posAtual)
                             posicoes.append(novaPos)
 
+                            # se o par de pontos for de extrusão, adicona as cores desse segmento
                             if movExtruCheck:
-                                color = self.mapaDeCores(
-                                    self.tempNormalizada(tempAtual))
+                                color = self.mapaDeCores(1.0)
                                 colors.append(color)
                                 colors.append(color)
+
+                            # se o par de pontos for de travel, adicona as cores desse segmento
                             else:
-                                travel_color = (0.5, 0.5, 1.0, 0.3)
+                                travel_color = self.mapaDeCores(0.0)
                                 colors.append(travel_color)
                                 colors.append(travel_color)
 
+                        # atualiza a posAtual
                         posAtual = novaPos
+
+                        # atualiza a altura
                         if novaPos[2] > zMax:
                             zMax = novaPos[2]
-                    self.comandosGcodeParaEnviar.append(linhaStrip)
-                    self.totalMovimentos += 1
-
-            if tempMin >= tempMax:
-                self.tempNormalizada = plt.Normalize(vmin=20, vmax=250)
-            else:
-                self.tempNormalizada = plt.Normalize(
-                    vmin=tempMin, vmax=tempMax)
 
             self.gCodePosData = np.array(posicoes, dtype=np.float32)
             self.gCodeCorData = np.array(colors, dtype=np.float32)
@@ -206,6 +215,7 @@ class GcodeWindow:
                 connect='segments'
             )
 
+            # Ajuste de camera
             x_min, y_min, z_min = np.min(self.gCodePosData, axis=0)
             x_max, y_max, z_max = np.max(self.gCodePosData, axis=0)
 
@@ -220,6 +230,7 @@ class GcodeWindow:
             self.vispy_view.camera.center = centro_peca
             self.vispy_view.camera.distance = tamanho_peca * 2
 
+            # slider de camadas
             totalVertices = len(self.gCodePosData)
             self.layerSlider.config(to=totalVertices)
             self.layerSliderVar.set(totalVertices)
--- slicing/Interface/interfaceTkinter.py
+++ slicing/Interface/interfaceTkinter.py
@@ -1,15 +1,15 @@
 import tkinter as tk
 from tkinter import ttk, scrolledtext, messagebox, filedialog
-from vispy.scene import visuals, SceneCanvas
-from vispy.app import use_app
+# from vispy.scene import visuals, SceneCanvas
+# from vispy.app import use_app
 import os
 import sys
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import re
-import numpy as np
-import matplotlib.cm as cm
+# import matplotlib.pyplot as plt
+# from matplotlib.animation import FuncAnimation
+# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+# import re
+# import numpy as np
+# import matplotlib.cm as cm
 import sv_ttk
 from gcodewindow import GcodeWindow
 
@@ -21,14 +21,14 @@ else:
     ASSET_PATH = APP_PATH
 
 
-class ParametersWindow(ttk.Frame):
-    def __init__(self, mWindow):
-        super().__init__(mWindow)
+# class ParametersWindow(ttk.Frame):
+#     def __init__(self, mWindow):
+#         super().__init__(mWindow)
 
 
-class StlWindow(ttk.Frame):
-    def __init__(self, mWindow):
-        super().__init__(mWindow)
+# class StlWindow(ttk.Frame):
+#     def __init__(self, mWindow):
+#         super().__init__(mWindow)
 
 
 class MainWindow:
@@ -39,20 +39,22 @@ class MainWindow:
         self.window.geometry("800x600")
         self.style = ttk.Style(self.window)
         self.tema = "light"
+
+        # aplicar tema
         sv_ttk.set_theme(self.tema)
 
         # tela-container na mainWindow pra add os botões
-        conTela = ttk.Frame(self.window, padding="10")
-        conTela.pack(fill='x', side='top')
-        conTela.columnconfigure(1, weight=1)
-        conTela.columnconfigure(3, weight=1)
+        self.conTela = ttk.Frame(self.window, padding="10")
+        self.conTela.pack(fill='x', side='top')
+        self.conTela.columnconfigure(1, weight=1)
+        self.conTela.columnconfigure(3, weight=1)
 
         # Making a Notebook
         self.notebook = ttk.Notebook(window)
         self.notebook.pack(expand=True, fill='both',
                            side='top', pady=5, padx=10)
 
-        # Making aux-windows
+        # Making sub-windows
         # self.w1 = ParametersWindow(self.notebook)
         # self.w2 = StlWindow(self.notebook)
         self.w3 = GcodeWindow(self.notebook, self.window)
@@ -67,7 +69,7 @@ class MainWindow:
             file=os.path.join(ASSET_PATH, "D:\Github\_new_version_altprint\slicing\Interface\mudarTema.png"))
 
         self.botaoTema = ttk.Button(
-            conTela, image=self.iconeTema, command=lambda: self.mudarTema(self.w3))
+            self.conTela, image=self.iconeTema, command=lambda: self.mudarTema(self.w3))
         self.botaoTema.grid(row=0, column=6, padx=5, pady=5)
 
     def mudarTema(self, gcodewindow: GcodeWindow):
